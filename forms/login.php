fe5c523da3bff9bf17005b1c959399b8256bbf32
mfaulan <48345951+mklfln@users.noreply.github.com>
2020-04-10 21:29:49 +0800

    Update login.php

--- forms/login.php
+++ forms/login.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+include('../db/connection.php');
+
+$db = new db();
 
     // form variables
     $form_USERNAME = $_POST['USERNAME'];
@@ -6,10 +10,30 @@
 
     try {
         // sql here
+        $stmt = $db->connection->prepare("SELECT username FROM users WHERE username = :username "); 
+        $stmt->execute(array(':username'=>$form_USERNAME));
+        $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+        
 
+        
+       if($stmt->rowCount() > 0){
+        if(password_verify($form_PASSWORD, $userRow['password'])){
+            //session variables
+            $_SESSION['user_id'] = $userRow['user_id'];
+            $_SESSION['username'] = $userRow['username'];   
+
+         header('location: ./user-home.php');
+    }
+    else
+    {
         echo json_encode(array(
-            'result' => $form_PASSWORD,
+            'error' => array(
+                'msg' => 'Incorrect username or password'
+            ),
         ));
+    }
+}
+
     } catch (Exception $e) {
         echo json_encode(array(
             'error' => array(
@@ -18,3 +42,4 @@
             ),
         ));
     }
+ ?> 
