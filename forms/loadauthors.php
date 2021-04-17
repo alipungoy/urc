0587bbb12dce1d9d500c8b678fa3a4b4a2332fe5
mfaulan <48345951+mklfln@users.noreply.github.com>
2021-04-18 01:10:21 +0800

    To change flow of research upload

--- forms/loadauthors.php
+++ forms/loadauthors.php
@@ -4,70 +4,40 @@ $test = $_SESSION['loggedin'];
 include('../db/connection.php');
 $db = new db();
 
-$draw = $_POST['draw'];
-$row = $_POST['start'];
-$rowperpage = $_POST['length'];
-$columnIndex = $_POST['order'][0]['column'];
-$columnName = $_POST['columns'][$columnIndex]['data'];
-$columnSortOrder = $_POST['order'][0]['dir'];
-$searchValue = $_POST['search']['value'];
 
-$searchArray = array();
+$numberofrecords = 10;
 
+if(!isset($_POST['searchTerm'])){
 
-## Search 
-$searchQuery = " ";
-if($searchValue != ''){
-   $searchQuery = " AND (fullname LIKE :fullname) ";
-   $searchArray = array( 
-        'fullname'=>"%$searchValue%", 
+   // Fetch records
+   $stmt = $db->connection->prepare("SELECT * FROM authors ORDER BY fullname LIMIT :limit");
+   $stmt->bindValue(':limit', (int)$numberofrecords, PDO::PARAM_INT);
+   $stmt->execute();
+   $name = $stmt->fetchAll();
+
+}else{
+
+   $search = $_POST['searchTerm'];// Search text
+
+   // Fetch records
+   $stmt = $db->connection->prepare("SELECT * FROM authors WHERE fullname like :fullname ORDER BY fullname LIMIT :limit");
+   $stmt->bindValue(':fullname', '%'.$search.'%', PDO::PARAM_STR);
+   $stmt->bindValue(':limit', (int)$numberofrecords, PDO::PARAM_INT);
+   $stmt->execute();
+   $name = $stmt->fetchAll();
+
+}
+
+$response = array();
+
+// Read Data
+foreach($name as $authors){
+   $response[] = array(
+     "id" => $authors['authorID'],
+      "text" => $authors['fullname']
    );
 }
 
-## Total number of records without filtering
-$stmt = $db->connection->prepare("SELECT COUNT(*) AS allcount FROM authors");
-$stmt->execute();
-$records = $stmt->fetch();
-$totalRecords = $records['allcount'];
-
-## Total number of records with filtering
-$stmt =  $db->connection->prepare("SELECT COUNT(*) AS allcount FROM authors WHERE 1 ".$searchQuery);
-$stmt->execute($searchArray);
-$records = $stmt->fetch();
-$totalRecordwithFilter = $records['allcount'];
-
-$stmt =  $db->connection->prepare("SELECT * FROM authors WHERE 1 ".$searchQuery." ORDER BY ".$columnName." ".$columnSortOrder." LIMIT :limit,:offset");
-
-// Bind values
-foreach($searchArray as $key=>$search){
-  $stmt->bindValue(':'.$key, $search,PDO::PARAM_STR);
-}
-
-$stmt->bindValue(':limit', (int)$row, PDO::PARAM_INT);
-$stmt->bindValue(':offset', (int)$rowperpage, PDO::PARAM_INT);
-$stmt->execute();
-$empRecords = $stmt->fetchAll();
-
-$data = array();
-
-foreach($empRecords as $row){
-  $data[] = array(
-     "fullname"=>$row['fullname'],
-     "mainAuthCb"=> '<input type ="checkbox" id="mainAuthCb" value='.$row['authorID'].' PID='.$row['authorID'].'>',
-     "coAuthCb"=> '<input type ="checkbox" id="coAuthCb" value='.$row['authorID'].' PID='.$row['authorID'].'>'
-  );
-}
-
-
-#response
-$response = array(
-  "draw" => intval($draw),
-  "iTotalRecords" => $totalRecords,
-  "iTotalDisplayRecords" => $totalRecordwithFilter,
-  "aaData" => $data
-
-);
-    
 echo json_encode($response);
-    
+exit();
 ?>
--- sidenav.php
+++ sidenav.php
@@ -4,6 +4,7 @@ require_once('./include/navheader.php');
 
 <!DOCTYPE html>
 <html>
+<link rel="stylesheet" type="text/css" href="./css/sidenav.css">
 <body>
 <?php
  require_once('./include/sidebar.php');
