f590b95bd34e24ff443fe908d8c9ac8da721a073
crrmacarse <crrmacarse@gmail.com>
2021-12-06 15:29:23 +0800

    feat: added support to delete news

---
+++ api/delete/news.php
@@ -0,0 +1,28 @@
+<?php
+include('../../db/connection.php');
+$db = new db();
+
+// Allow post request only
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    // Validate for required fields
+     // Validate for required fields
+     if (!isset($_GET["id"])) {
+        $output = json_encode(array('type' => 'error', 'message' => 'Id not found!'));
+        die($output);
+    }
+
+    // Assigned fields
+    $FORM_ID = $_GET['id'];
+
+    try {
+        $sql = "DELETE FROM events where id =:id";
+        $stmt = $db->connection->prepare($sql);
+        $stmt->bindParam(':id', $FORM_ID, PDO::PARAM_INT);
+        $stmt->execute();
+
+        die(json_encode(array('type' => 'success', 'message' => 'Succesfully Deleted News')));
+    } catch (\Throwable $th) {
+        $output = json_encode(array('type' => 'error', 'message' => $th->getMessage()));
+        die($output);
+    }
+}
