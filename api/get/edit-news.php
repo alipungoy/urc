aa265444a412f80a4eaadb1774f23cce6e5c4648
Mikel Faulan <mfaulan@gmail.com>
2023-07-20 06:56:18 +0800

    Finish journals upload module

--- api/get/edit-news.php
+++ api/get/edit-news.php
@@ -3,22 +3,26 @@ include('../../db/connection.php');
 $db = new db();
 $events = array();
 
-// Allow get request only
+// Allow post request only
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     try {
-        $show = ("SELECT * FROM events");
+        // Get the ID from the URL using $_GET
+        $id = $_GET['id'];
+        $show = "SELECT * FROM urc_news WHERE id = :id"; // Use prepared statement with a placeholder for the ID
         $stmt = $db->connection->prepare($show);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT); // Bind the ID value to the placeholder
         $stmt->execute();
         $row = $stmt->fetchAll();
-        
+
         foreach ($row as $rows) {
-            $events[] = (array('id' => $rows['id'],
-                            'title' => $rows['event_title'],
-                            'details' => $rows['events_information'],
-                            'start' => $rows['event_from_time'],
-                            'end' => $rows['event_to_time']));
-        };
-        
+            $events[] = array(
+                'id' => $rows['id'],
+                'title' => $rows['title'],
+                'details' => $rows['details'],
+                'date' => $rows['date'],
+            );
+        }
+
         $totalData = $stmt->rowCount();
 
         $json_data = array(
@@ -31,4 +35,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         die($e->getMessage());
     }
 }
-
+?>
--- api/get/load-news.php
+++ api/get/load-news.php
@@ -3,22 +3,23 @@ include('../../db/connection.php');
 $db = new db();
 $events = array();
 
-// Allow get request only
+// Allow post request only
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     try {
-        $show = ("SELECT * FROM events");
+        $show = ("SELECT * FROM urc_news");
         $stmt = $db->connection->prepare($show);
         $stmt->execute();
         $row = $stmt->fetchAll();
-        
+
         foreach ($row as $rows) {
-            $events[] = (array('id' => $rows['id'],
-                            'title' => $rows['event_title'],
-                            'details' => $rows['events_information'],
-                            'start' => $rows['event_from_time'],
-                            'end' => $rows['event_to_time']));
+            $events[] = (array(
+                'id' => $rows['id'],
+                'title' => $rows['title'],
+                'details' => $rows['details'],
+                'date' => $rows['date']
+            ));
         };
-        
+
         $totalData = $stmt->rowCount();
 
         $json_data = array(
@@ -31,4 +32,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         die($e->getMessage());
     }
 }
-
